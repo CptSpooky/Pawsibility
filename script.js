8387b3eddab263dcd7705df35383fcf65bc78dbe
Garrett Hudson <garretthudson87@yahoo.com>
2020-08-03 17:39:54 -0400

    added variables to select the inputs from our form and the submit button. added an onclick function to take those variables and save them to the local storage when the submit button was clicked. Also added the basic outline of the mapbox API call and ajax

--- script.js
+++ script.js
@@ -9,15 +9,58 @@
 
 $(document).ready(function() {
 // global variables
-var zipCode = $("#userZip").val();
-var species = $("#speciesCheck").val();
-var breed = $("#inputBreed").val();
-var age = $("#ageCheck").val();
-var gender = $("").val();
-var submitBtn = $("#critSubmit");
+  var speciesValue = $("input[name='exampleRadios']:checked").val();
+  var zipCode = $("#userZip").val();
+  var speciesValue = $("input[name='exampleRadios']:checked").val();
+  var breed = $("#inputBreed").val();
+  // var ages;
+  var gender = $("#genderSelect option:selected").val();
+  var submitBtn = $("#critSubmit");
+
+submitBtn.on("click", function() {
+  var zipCode = $("#userZip").val();
+  var speciesValue = $("input[name='exampleRadios']:checked").val();
+  var breed = $("#inputBreed").val(); 
+  var gender = $("#genderSelect option:selected").val();
+  // var ages;
+  var submitBtn = $("#critSubmit");
+
+  let ages = $("input[type='checkbox']:checked").map(function(){return $(this).val()}).get();
+  
+  
+
+  console.log(zipCode);
+  console.log(speciesValue);
+  console.log(breed);
+  console.log(ages); // console log age to test
+  console.log(gender);
+
+  // save inputs into local storage
+localStorage.setItem('zip code: ', zipCode);
+localStorage.setItem('species: ', speciesValue);
+localStorage.setItem('breed: ', breed);
+localStorage.setItem('ages: ', ages); // is returning the value [object Object] in the local storage instead of the array of checkboxes
+localStorage.setItem('gender: ', gender);
+})
+
+
+
+
+
+
+
 
 // mapbox API call
-var mapboxURL = ""
+var apiToken = "pk.eyJ1IjoiY3B0c3Bvb2t5IiwiYSI6ImNrZDlpcDRheDA0b2IzM2pxZDZzNnI2Y2cifQ.0GQCDJlDIwPOy_9uR0Vgsw";
+var searchValue = $(""); // add the address of the shelter from data pulled from petfinder APU
+var mapboxURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + searchValue + ".json?" + apiToken;
+
+$.ajax({
+  url: mapboxURL,
+  method: 'GET'
+}).then(function() {
+  
+})
 
 
 
