fcdbd70f573fd2b356e082d1e003951f6dfdac34
CptSpooky <legacyofthedonut@gmail.com>
2020-08-08 19:33:38 -0400

    fixed script page issues with API

--- script.js
+++ script.js
@@ -6,65 +6,52 @@ $(document).ready(function() {
   var submitBtn = $("#critSubmit");
 
   // petfinder API Post access call
-  function petFinderAccess() {
     $.ajax({
       type: "POST",
       url: "https://api.petfinder.com/v2/oauth2/token",
       data: "grant_type=client_credentials&client_id=VPazYlCS5jiHVKYEa29a1eAD74ZNDsYvXJ0JYtdykdrfmRjsiC&client_secret=EyTeV2BZo5Nve7dLybgTiCKaZ4GxNA9pFcIoaGGu",
       success: function(response) {
         petToken = response.access_token; //Generating token because it expires in an hour, would normally be handled server side 
+        //Default dog
+        populateBreeds("dog");
       }
     });
-  }  
-
-  petFinderAccess(); //Api call
-  breedSelection();
-
-  // Generates the breed list dependent on radio button selection
-  function breedSelection(){
-
-    //Default dog
+    
+  //Breeds button activation
+  $("#inline_test input[name='exampleRadios']").click(function(){
     var animal = $('input:radio[name=exampleRadios]:checked').val();
     populateBreeds(animal);
+  });
 
-    //Breeds button activation
-    $("#inline_test input[name='exampleRadios']").click(function(){
-      
-      var animal = $('input:radio[name=exampleRadios]:checked').val();
-      populateBreeds(animal);
+  //Breeds selection and generation
+  function populateBreeds(animal){
+    $.ajax({
+      type: "GET",
+      url: "https://api.petfinder.com/v2/types/" + animal + "/breeds",    
+      headers: {"Authorization": "Bearer " + petToken},
+      success: function(response) {
+            
+        var breedSelect = $("#breedSelect");
+        breedSelect.empty(); //clear out existing options
+        
+        //any breed
+        var anyBreed = document.createElement("option");
+        anyBreed.value = "Any";
+        anyBreed.innerHTML = "Any";
+        breedSelect.append(anyBreed);
+
+        //generate breeds
+        response.breeds.forEach(e => {
+        
+          var opt = document.createElement("option");
+          opt.value = e.name;
+          opt.innerHTML= e.name;
+          breedSelect.append(opt);
+  
+        });
+      }
     });
-
-    //Breeds selection and generation
-    function populateBreeds(animal){
-      $.ajax({
-        type: "GET",
-        url: "https://api.petfinder.com/v2/types/" + animal + "/breeds",    
-        headers: {"Authorization": "Bearer " + petToken},
-        success: function(response) {
-              
-          var breedSelect = $("#breedSelect");
-          breedSelect.empty(); //clear out existing options
-          
-          //any breed
-          var anyBreed = document.createElement("option");
-          anyBreed.value = "Any";
-          anyBreed.text = "Any";
-          breedSelect.append(anyBreed);
-
-          //generate breeds
-          response.breeds.forEach(e => {
-          
-            var opt = document.createElement("option");
-            opt.value = e.name;
-            opt.innerHTML= e.name;
-            breedSelect.append(opt);
-    
-          });
-        }
-      });
-    }
   }
-
   
   //When submit button is clicked
   submitBtn.on("click", function(event){
@@ -75,9 +62,7 @@ $(document).ready(function() {
     var breed = $("#breedSelect").val(); 
     var gender = $("#genderSelect option:selected").val();
     let ages = $("input[type='checkbox']:checked").map(function(){return $(this).val()}).get();
-    
-    console.log(zipCode.length);
-     
+         
     if (isNaN(parseInt(zipCode)) || zipCode.length != 5) {
       $("#userZip").val("");
       $("#userZip").addClass("submit-fail");
